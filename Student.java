2c8635e6fd09c0e916bce00f9fff947fb968aee3
Danae <danaero25@gmail.com>
2025-11-27 16:05:26 -0600

    Se implementaron los métodos en la clase Student

--- Student.java
+++ Student.java
@@ -1,9 +1,21 @@
 
 public class Student {
+  String firstName;
+  String lastName;
+  int registration;
+  int grade;
+  int year;
+  public void printFullName(){
+      //TODO implement
+   }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+   public void isApproved(){
+       //TODO implement: should return true if grade >= 60
+   }
 
-	}
-
-}
+   public int changeYearIfApproved(){
+       //TODO implement: the student should advance to the next year if he/she grade is >= 60
+       // Make year = year + 1, and print "Congragulations" if the student has been approved
+       return 0;
+   }
+}// class Student
